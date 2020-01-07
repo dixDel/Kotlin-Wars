bbc80e1d06a16f15548d2b598958f50ab4171df8
Didier Delhaye <didier.delhaye@drkdidel.be>
2020-01-07 11:21:30 +0100

    scroll to next match

--- app/src/main/java/be/drkdidel/dixdel/kotlinwars/MainActivity.kt
+++ app/src/main/java/be/drkdidel/dixdel/kotlinwars/MainActivity.kt
@@ -2,13 +2,13 @@ package be.drkdidel.dixdel.kotlinwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Visibility
 import android.view.View
 import android.widget.ScrollView
-import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var previousPositionMatch = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         searchButton.setOnClickListener {
             //@todo case insensitive
-            //  scroll to first match
-            //  then next
-            var search = fightOutputTextView.text.indexOf(searchEditText.text.toString())
-            var lineNumber = fightOutputTextView.layout.getLineForOffset(search)
+            //  search backwards
+            // search from current scroll position
+            var positionMatch = fightOutputTextView.text.indexOf(searchEditText.text.toString(), previousPositionMatch)
+            previousPositionMatch = positionMatch + 1
+            var lineNumber = fightOutputTextView.layout.getLineForOffset(positionMatch)
             scrollView.scrollTo(0, fightOutputTextView.layout.getLineTop(lineNumber))
         }
 
